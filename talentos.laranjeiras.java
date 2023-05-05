4c6fe8571bc92bbf369fd32b55f317d7ee80d870
elias <eliasvinharski19@gmail.com>
2023-05-05 09:06:50 -0300

    -q

---
+++ talentos.laranjeiras.java
@@ -0,0 +1,33 @@
+
+public class Pessoas () {
+   private String nome;
+   private String sobrenome;
+   private Integer idade;
+
+   public void set nome (String nome) {
+      this._nome = nome;
+   }
+
+
+   public void set sobrenome (String sobrenome) {
+      this._sobrenome = sobrenome;
+   }
+
+
+   public void set idade (Integer idade) {
+      this._idade = idade;
+   }
+
+   public String get nome () {
+      return this._nome;
+   }
+
+   public String get sobrenome () {
+      return this._sobrenome;
+   }
+
+   public Integer get idade () {
+      return this._idade;
+   }
+
+}
---
+++ teste.js
@@ -0,0 +1,6 @@
+
+function Pessoas () {
+    this._nome = null;
+    this._sobrenome = null;
+    this._idade = null;
+}
